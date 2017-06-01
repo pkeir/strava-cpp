ed7724281ed08caf6dca93d2e51b8341561fc0cc
william-taylor <wi11berto@yahoo.co.uk>
2017-06-01 21:51:40 +0100

    basic template

---
+++ samples/sample01/main.cpp
@@ -0,0 +1,8 @@
+
+#include <strava.hpp>
+#include <iostream>
+
+int main(int argc, char * argv[])
+{
+    std::cout << "Sample01" << std::endl;
+}
---
+++ source/src/strava.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include <strava.hpp>
+
+int main(int argc, char * argv[])
+{
+  std::cout << message << std::endl;
+}
