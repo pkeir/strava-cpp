aecbd892fa25f5ba4a6953754f9a479888bbce6c
william-taylor <wi11berto@yahoo.co.uk>
2017-06-05 00:03:50 +0100

    removing cpr, no https support, now using curl

--- samples/https/main.cpp
+++ samples/https/main.cpp
@@ -1,10 +1,8 @@
 
-#include <cpr/cpr.h>
 #include <iostream>
 
 int main(int argc, const char* argv[])
 {
-    auto response = cpr::Get(cpr::Url{ "http://www.williamsamtaylor.co.uk:3001/" });
-    std::cout << response.text << std::endl;
+    std::cout << "Hello, World" << std::endl;
     std::cin.get();
 }
