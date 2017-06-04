c1a3305234bfd61ee773f2faceef8a191dc22a13
william-taylor <wi11berto@yahoo.co.uk>
2017-06-04 23:42:55 +0100

    working http version

--- samples/https/main.cpp
+++ samples/https/main.cpp
@@ -1,10 +1,10 @@
-#include <cpr/cpr.h>
 
-int main(int argc, char** argv) {
-    auto r = cpr::Get(cpr::Url{ "https://api.github.com/repos/whoshuu/cpr/contributors" },
-        cpr::Authentication{ "user", "pass" },
-        cpr::Parameters{ { "anon", "true" },{ "key", "value" } });
-    r.status_code;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    r.text;                         // JSON text string
+#include <cpr/cpr.h>
+#include <iostream>
+
+int main(int argc, const char* argv[])
+{
+    auto response = cpr::Get(cpr::Url{ "http://www.williamsamtaylor.co.uk:3001/" });
+    std::cout << response.text << std::endl;
+    std::cin.get();
 }
