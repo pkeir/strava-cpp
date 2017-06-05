7e1d39c95c083f63a86ae54874539aabadf74a4d
william-taylor <wi11berto@yahoo.co.uk>
2017-06-05 17:27:16 +0100

    altering makelists to link to poco

--- samples/helloworld/main.cpp
+++ samples/helloworld/main.cpp
@@ -1,8 +1,13 @@
 
+#include <strava.hpp>
 #include <iostream>
 
 int main(int argc, char*argv[])
 {
     std::cout << "Hello, World!" << std::endl;
     std::cin.get();
+
+    // get athlete
+    // print age
+    // print location
 }
