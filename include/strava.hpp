984dba99daca87ad8ab834d3fcbd099b296d3319
william-taylor <wi11berto@yahoo.co.uk>
2017-06-11 19:53:13 +0100

    adding placeholder types

--- include/strava.hpp
+++ include/strava.hpp
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace strava
 {
@@ -13,6 +14,43 @@ namespace strava
         std::string client_id;
     };
 
+    struct bike
+    {
+        std::string id;
+        bool primary;
+        std::string name;
+        double distance;
+        int resource_state;
+    };
+
+    struct club
+    {
+        int id;
+        int resource_state;
+        std::string name;
+        std::string profile_medium;
+        std::string profile;
+        std::string cover_photo;
+        std::string cover_photo_small;
+        std::string sport_type;
+        std::string city;
+        std::string state;
+        std::string country;
+        bool is_private;
+        int member_count;
+        bool featured;
+        std::string url;
+    };
+
+    struct shoe
+    {
+        std::string id;
+        bool primary;
+        std::string name;
+        double distance;
+        int resource_state;
+    };
+
     struct athlete
     {
         int id;
@@ -21,6 +59,28 @@ namespace strava
         std::string lastname;
         std::string profile_medium;
         std::string profile;
+        std::string city;
+        std::string state;
+        std::string country;
+        std::string sex;
+        std::string is_friend;
+        std::string follower;
+        bool premium;
+        time_t created_at;
+        time_t updated_at;
+        int follower_count;
+        int friend_count;
+        int mutual_friend_count;
+        int athlete_type;
+        std::string date_preference;
+        std::string measurement_preference;
+        std::string email;
+        int ftp;
+        int weight;
+
+        std::vector<club> clubs;
+        std::vector<bike> bikes;
+        std::vector<shoe> shoes;
     };
 
     namespace athletes
--- samples/dev/main.cpp
+++ samples/dev/main.cpp
@@ -5,15 +5,16 @@
 int main(int argc, char*argv[])
 {
     strava::authenticate({
-        "1fbc8877efd758b7744774a1217bab6e864e251b",
-        "<redirect_url>",
-        "<client_secret>",
-        "<client_id>"
+        "1fbc8877efd758b7744774a1217bab6e864e251b",  // access_token
+        "http://localhost:3000",                     // redirect_url
+        "8a08050aaf532074ab06bdacf3297b3ecc86d640 ", // client_secret
+        "18035"                                      // client_id
     });
     
     strava::athlete me;
     strava::athletes::current(me);
 
-    std::cout << me.firstname << " " << me.lastname << std::endl;
+    std::cout << me.firstname << " ";
+    std::cout << me.lastname << std::endl;
     std::cin.get();
 }
