e53d0462454a86811b33e483615845333bddb909
william-taylor <wi11berto@yahoo.co.uk>
2017-06-14 20:21:59 +0100

    updating athlete test + cmake build

--- tests/athlete-tests/main.cpp
+++ tests/athlete-tests/main.cpp
@@ -1,17 +1,29 @@
 
+#include <strava.hpp>
 #include <lest.hpp>
 
 using namespace std;
 
 const lest::test specification[] =
 {
-    CASE("Basic Zero Test")
+    CASE("Athlete name test")
     {
-        EXPECT(0 == 0);
+        strava::detailed::athlete me;
+        strava::athlete::current(me);
+
+        EXPECT(!me.firstname.empty());
+        EXPECT(!me.lastname.empty());
     }
 };
 
 int main(int argc, char * argv[])
 {
+    strava::authenticate({
+        "1fbc8877efd758b7744774a1217bab6e864e251b",  // access_token
+        "http://localhost:3000",                     // redirect_url
+        "8a08050aaf532074ab06bdacf3297b3ecc86d640 ", // client_secret
+        "18035"                                      // client_id
+    });
+
     return lest::run(specification, argc, argv);
 }
